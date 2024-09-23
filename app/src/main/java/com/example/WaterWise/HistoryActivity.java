0773b4be1255f6baa21a71ea649e3391b27fcec4
Tayebe kavousi <tayebekavousi68@gmail.com>
2024-09-23 15:24:57 +0800

    showing date and time in history page

--- app/src/main/java/com/example/WaterWise/HistoryActivity.java
+++ app/src/main/java/com/example/WaterWise/HistoryActivity.java
@@ -11,10 +11,12 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class HistoryActivity extends AppCompatActivity {
 
-    private TextView dayOfWeekTextView;
+    private FirestoreHelper firestoreHelper;
+    private TextView dayOfWeekTextView, dateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +24,12 @@ public class HistoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_history);
 
         dayOfWeekTextView = findViewById(R.id.dayOfWeekTextView);
+        dateTextView = findViewById(R.id.dateTextView);
+        firestoreHelper = new FirestoreHelper();
+
         displayDayOfWeek();
 
+        // Bottom NavBar
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         Menu menu = bottomNavigationView.getMenu();
         menu.findItem(R.id.nav_home).setVisible(true);
@@ -41,8 +47,15 @@ public class HistoryActivity extends AppCompatActivity {
     }
 
     public void displayDayOfWeek() {
+        Date currentDate = new Date();
+
         SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
         String currentDay = sdf.format(new Date());
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy", Locale.getDefault());
+        String formattedDate = dateFormat.format(currentDate);
+
         dayOfWeekTextView.setText(currentDay);
+        dateTextView.setText(formattedDate);
     }
 }
