e34b06bd74691ec9f016e3d83de9dd0c37c5ec35
Tayebe kavousi <tayebekavousi68@gmail.com>
2024-09-23 12:08:14 +0800

    History page initiated

--- app/src/main/java/com/example/WaterWise/HistoryActivity.java
+++ app/src/main/java/com/example/WaterWise/HistoryActivity.java
@@ -1,4 +1,48 @@
 package com.example.WaterWise;
 
-public class HistoryActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    private TextView dayOfWeekTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        dayOfWeekTextView = findViewById(R.id.dayOfWeekTextView);
+        displayDayOfWeek();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        Menu menu = bottomNavigationView.getMenu();
+        menu.findItem(R.id.nav_home).setVisible(true);
+        menu.findItem(R.id.nav_add_water).setVisible(false);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_settings) {
+                startActivity(new Intent(HistoryActivity.this, SettingsActivity.class));
+                return true;
+            } else if (itemId == R.id.nav_home) {
+                startActivity(new Intent(HistoryActivity.this, MainActivity.class));
+                return true;
+            } else return itemId == R.id.nav_history;
+        });
+    }
+
+    public void displayDayOfWeek() {
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
+        String currentDay = sdf.format(new Date());
+        dayOfWeekTextView.setText(currentDay);
+    }
 }
